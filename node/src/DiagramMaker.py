5e8210a7fae84b9b406edb5a1a7ea1b6c38e4783
Oscar Cadena <oscar7913@gmail.com>
2024-03-23 15:10:24 -0400

    Update clean folders

--- node/src/DiagramMaker.py
+++ node/src/DiagramMaker.py
@@ -1,27 +1,34 @@
-from diagrams import Cluster,Diagram, Edge
-from diagrams.aws.compute import EC2, ECS, EKS, Lambda
-from diagrams.aws.database import RDS, ElastiCache, RDS, Redshift
-from diagrams.aws.network import ELB, Route53
-from diagrams.aws.integration import SQS
-from diagrams.aws.storage import S3
+# from diagrams import Cluster,Diagram, Edge
+# from diagrams.aws.compute import EC2, ECS, EKS, Lambda
+# from diagrams.aws.database import RDS, ElastiCache, RDS, Redshift
+# from diagrams.aws.network import ELB, Route53
+# from diagrams.aws.integration import SQS
+# from diagrams.aws.storage import S3
 import sys
+import re
+import json
 
 
 def generate_diagram():
     try:
-        filename = '/shared/diagram'
-        print(sys.argv[1])
-        # if len(sys.argv[1]) == 0: 
-        #     with Diagram("Web Service", filename=filename, show=False):
-        #         pass
-        # else:
-        code_to_execute = sys.argv[1]
-        with Diagram("Web Service", filename=filename, show=False):
-            exec(code_to_execute)   
-        print('{"message": "ok", "status": 200}')
+        filename, file_path = refactor_code(sys.argv[1])
+        exec(filename)
+        print('{"message":'+f'"{file_path}"'+', "status": 200}')
     except Exception as e:
         print('{"message":"' + str(e)+'", "status": 500}')
 
 
+def refactor_code(code):
+    pattern = r'(with Diagram\([^)]*,\s*filename\s*=\s*[\'\"])([^\'\"]+)([\'\"])'
+    match = re.search(pattern, code)
+    if match:
+        filename_value = match.group(2)
+        replacement = match.group(1) + "/shared/" + filename_value + match.group(3)
+        return re.sub(pattern, replacement, code), filename_value
+    else:
+        return code, "empty"
+
+
 if __name__ == "__main__":
     generate_diagram()
+
